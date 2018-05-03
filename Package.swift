02841731984a9d291ab322183771c5469ebea128
Hulk <hulk104255@gmail.com>
2018-05-03 23:12:15 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,5 @@ let package = Package(
     name: "OpenSSL",
     dependencies: [
         .Package(url: "https://github.com/Zewo/Axis.git", majorVersion: 0, minor: 14),
-        .Package(url: "https://github.com/Zewo/COpenSSL.git", majorVersion: 0, minor: 14)
     ]
 )
